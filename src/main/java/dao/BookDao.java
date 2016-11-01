c518f910d8da75911e862ec5e0d72c7c1b56ccca
Eduards Morozovs <ed.morozovs@gmail.com>
2016-11-01 11:11:28 +0200

    addBookDao

--- src/main/java/dao/BookDao.java
+++ src/main/java/dao/BookDao.java
@@ -3,14 +3,25 @@ package dao;
 
 import model.Book;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 /**
  * Created by student on 16.1.11.
  */
-public class BookDao{
 
-    public boolean takeBook(Book book){
+
+public class BookDao implements IDao {
+
+    private EntityManagerFactory factory;
+
+    public BookDao(EntityManagerFactory factory) {
+        this.factory = factory;
+    }
+
+    public boolean takeBook(Book book) {
         return true;
     }
 
@@ -26,25 +37,55 @@ public class BookDao{
         return null;
     }
 
-    public List<Book> searchByYear (int year){
+    public List<Book> searchByYear(int year) {
         return null;
     }
 
 
-    public List<Book> recommendedBooks (String genreBook) {
+    public List<Book> recommendedBooks(String genreBook) {
         return null;
     }
 
     public List<Book> showAllBooks() {
-        return null;
+
+        EntityManager manager = factory.createEntityManager();
+        try {
+            TypedQuery<Book> query = manager.createQuery("SELECT b FROM Book b", Book.class);
+
+            return query.getResultList();
+
+        } finally {
+            manager.close();
+        }
+
     }
 
-    public boolean editBookCount (int bookNumber) {
+    public boolean editBookCount(int bookNumber) {
         return false;
     }
 
-    public boolean deleteBookNumber (int bookNumber) {
+    public boolean deleteBookNumber(int bookNumber) {
         return false;
     }
 
+    @Override
+    public boolean add(Object obj) {
+
+
+        return false;
+    }
+
+    @Override
+    public boolean update(Object obj) {
+ //       if(book != null) {
+
+//        }
+
+        return false;
+    }
+
+    @Override
+    public boolean delete(Object obj) {
+        return false;
+    }
 }
