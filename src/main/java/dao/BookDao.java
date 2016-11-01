407fa8b7901b02cf18ec2ab7a33cca617a7ba9df
Eduards Morozovs <ed.morozovs@gmail.com>
2016-11-01 15:00:14 +0200

    addBookDao

--- src/main/java/dao/BookDao.java
+++ src/main/java/dao/BookDao.java
@@ -2,10 +2,13 @@ package dao;
 
 
 import model.Book;
+import model.Client;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transaction;
 import java.util.List;
 
 /**
@@ -13,38 +16,129 @@ import java.util.List;
  */
 
 
-public class BookDao implements IDao {
+public class BookDao implements IDao<Book> {
 
     private EntityManagerFactory factory;
+    private Client client;
 
-    public BookDao(EntityManagerFactory factory) {
+
+    public BookDao(EntityManagerFactory factory, Client client) {
         this.factory = factory;
+        this.client = client;
+
     }
 
     public boolean takeBook(Book book) {
-        return true;
+        EntityManager manager = factory.createEntityManager();
+        EntityTransaction transaction = manager.getTransaction();
+        book = manager.find(Book.class, book.getId());
+        if (book.getBookCount() !=0) {
+
+            client = manager.find(Client.class, client.getId());
+            List<Book> clientBooks = client.getTakenBooks();
+            clientBooks.add(book);
+            try {
+                transaction.begin();
+                client.setTakenBooks(clientBooks);
+                transaction.commit();
+                return true;
+            } catch (Exception e) {
+                transaction.rollback();
+                return false;
+            } finally {
+                manager.close();
+            }
+        }
+        return false;
+
     }
 
     public boolean returnBook(Book book) {
-        return false;
-    }
 
-    public int reservationBook(Book book) {
-        return 0;
+        EntityManager manager = factory.createEntityManager();
+        EntityTransaction transaction = manager.getTransaction();
+        book = manager.find(Book.class, book.getId());
+        client = manager.find(Client.class, client.getId());
+        List<Book> clientBooks = client.getHistory();
+        clientBooks.add(book);
+        try {
+            transaction.begin();
+            client.setHistory(clientBooks);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            transaction.rollback();
+            return false;
+        } finally {
+            manager.close();
+        }
+
+
+
     }
+/*
+    public int reservationBook(Book book) {
+
+        EntityManager manager = factory.createEntityManager();
+        EntityTransaction transaction = manager.getTransaction();
+        book = manager.find(Book.class, book.getId());
+        client = manager.find(Client.class, client.getId());
+        List<Book> clientBooks = client.getReservationBooks();
+        clientBooks.add(book);
+        try {
+            transaction.begin();
+            client.setReservationBooks(clientBooks);
+            transaction.commit();
+            return
+        }
+        return 0;
+    }*/
 
     public List<Book> searchBookTitle(String title) {
+        if (title != "") {
+            EntityManager manager = factory.createEntityManager();
+
+            try {
+                TypedQuery<Book> query = manager.createQuery("SELECT b FROM Book b WHERE b.title = title" , Book.class );
+                return query.getResultList();
+            } finally {
+                manager.close();
+            }
+        }
         return null;
     }
 
+
     public List<Book> searchByYear(int year) {
+        if (year != 0) {
+            EntityManager manager = factory.createEntityManager();
+
+            try {
+                TypedQuery<Book> query = manager.createQuery("SELECT b FROM Book b WHERE b.year = " + year , Book.class );
+                return query.getResultList();
+            } finally {
+                manager.close();
+            }
+        }
         return null;
     }
 
 
     public List<Book> recommendedBooks(String genreBook) {
+        if (genreBook != "") {
+            EntityManager manager = factory.createEntityManager();
+
+            try {
+                TypedQuery<Book> query = manager.createQuery("SELECT b FROM Book b WHERE b.genre = " + genreBook , Book.class );
+                return query.getResultList();
+            } finally {
+                manager.close();
+            }
+        }
         return null;
     }
+
+
 
     public List<Book> showAllBooks() {
 
@@ -61,31 +155,94 @@ public class BookDao implements IDao {
     }
 
     public boolean editBookCount(int bookNumber) {
+
         return false;
     }
 
     public boolean deleteBookNumber(int bookNumber) {
+
+        EntityManager manager = factory.createEntityManager();
+        EntityTransaction transaction = manager.getTransaction();
+//        Book originBook = manager.find(Book.class, book.getId());
+
+        return false;
+
+
+    }
+
+    @Override
+    public boolean add(Book book) {
+        if (book != null) {
+            EntityManager manager = factory.createEntityManager();
+            EntityTransaction transaction = manager.getTransaction();
+
+            try {
+                transaction.begin();
+                manager.persist(book);
+                transaction.commit();
+                return true;
+            } catch (Exception e) {
+                transaction.rollback();
+                return false;
+            } finally {
+                manager.close();
+            }
+
+        }
         return false;
     }
 
     @Override
-    public boolean add(Object obj) {
+    public boolean update(Book book) {
+        if (book != null) {
+            EntityManager manager = factory.createEntityManager();
+            EntityTransaction transaction = manager.getTransaction();
+            Book originBook = manager.find(Book.class, book.getId());
 
+            try {
+                transaction.begin();
+                originBook.setTitle(book.getTitle());
+                originBook.setAuthor(book.getAuthor());
+                originBook.setGenre(book.getGenre());
+                originBook.setYear(book.getYear());
+                manager.merge(originBook);
+                transaction.commit();
+
+                return true;
+            } catch (Exception e) {
+                transaction.rollback();
+                return false;
+            } finally {
+                manager.close();
+
+            }
+        }
 
         return false;
     }
 
     @Override
-    public boolean update(Object obj) {
- //       if(book != null) {
+    public boolean delete(Book book) {
+        if (book != null) {
+            EntityManager manager = factory.createEntityManager();
+            EntityTransaction transaction = manager.getTransaction();
+            book = manager.find(Book.class, book.getId());
+            try {
+                transaction.begin();
+                manager.remove(book);
+                transaction.commit();
 
-//        }
-
-        return false;
-    }
-
-    @Override
-    public boolean delete(Object obj) {
+                return true;
+            } catch (Exception e) {
+                transaction.rollback();
+                return false;
+            } finally {
+                manager.close();
+            }
+        }
         return false;
     }
 }
+
+
+
